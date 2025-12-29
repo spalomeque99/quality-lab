d20fbd85fb636b041fa3b87d6d7b9205ecdcb8b6
spalomeque99 <sergyps3@gmail.com>
2025-12-29 15:51:58 +0100

    Dia 10 - Test containers

---
+++ src/test/java/com/quality/quality_lab/integration/TaskIntegrationPostgresTest.java
@@ -0,0 +1,87 @@
+package com.quality.quality_lab.integration;
+
+import com.quality.quality_lab.application.TaskService;
+import com.quality.quality_lab.domain.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.temporal.ChronoUnit;
+
+@SpringBootTest
+@Testcontainers
+public class TaskIntegrationPostgresTest {
+
+    @Autowired
+    private TaskService taskService;
+
+    private static String TITLE = "Titulo";
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("qualitylab")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void datasourceProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        // Recomendado para tests reproducibles
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
+    }
+
+    @Test
+    void should_create_task_and_return_it() {
+        Task savedTask = taskService.createTask(TITLE);
+
+        Assertions.assertNotNull(savedTask.id());
+        Assertions.assertFalse(savedTask.done());
+        Assertions.assertNotNull(savedTask.createdAt());
+    }
+
+    @Test
+    void should_create_task_return_it_and_finalize(){
+        Task savedTask = taskService.createTask(TITLE);
+
+        Task finalizedTask = taskService.finalizeTaskById(savedTask.id());
+
+        Assertions.assertNotNull(finalizedTask.id());
+        Assertions.assertFalse(savedTask.done());
+        Assertions.assertTrue(finalizedTask.done());
+        Assertions.assertNotNull(savedTask.createdAt());
+        Assertions.assertEquals(finalizedTask.title(), savedTask.title());
+        //Se hace asi, porque al persistir hay fallos en milisegundos
+        Assertions.assertEquals(finalizedTask.createdAt().truncatedTo(ChronoUnit.MILLIS), savedTask.createdAt().truncatedTo(ChronoUnit.MILLIS));
+        Assertions.assertEquals(finalizedTask.id(), savedTask.id());
+    }
+
+    @Test
+    void should_throw_exception_when_finalize_inexistent_task(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            taskService.finalizeTaskById(200L);
+        });
+    }
+
+    @Test
+    void should_throw_exception_when_finalize_finalized_task(){
+
+        Task savedTask = taskService.createTask(TITLE);
+
+        Task finalizedTask = taskService.finalizeTaskById(savedTask.id());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            taskService.finalizeTaskById(finalizedTask.id());
+        });
+    }
+}
